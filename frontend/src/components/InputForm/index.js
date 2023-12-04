61b9eda2c7d3af98c2ffb157b71e8c30e470cc2c
santohack <santohack238@gmail.com>
2023-12-04 13:47:19 +0530

    validation

--- frontend/src/components/InputForm/index.js
+++ frontend/src/components/InputForm/index.js
@@ -2,17 +2,27 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-export default function InputForm({label,type ,id,varient,onChange}) {
+export default function InputForm({ label, type, error, helperText, id, varient, onChange, value, isRequired }) {
   return (
     <Box
-      
+
       sx={{
         '& > :not(style)': { m: 1, width: '60ch' },
       }}
-    
+
     >
-      <TextField type={type} label={label} id={id} variant={varient}  onChange={onChange} />
-     
+      <TextField
+        error={isRequired && error}
+        helperText={isRequired && helperText}
+        value={value}
+        required={isRequired}
+        type={type}
+        label={label}
+        id={id}
+        variant={varient}
+        onChange={onChange}
+      />
+
     </Box>
   );
 }
--- frontend/src/pages/HomeScreen/index.js
+++ frontend/src/pages/HomeScreen/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { HomeContainer } from '../../styles/HomeScreen';
 import InputForm from '../../components/InputForm';
-import { Button, FormControl, Typography , Snackbar ,FormControlLabel, Radio, RadioGroup, TextField, Select, MenuItem, InputLabel, Box, Alert } from '@mui/material';
+import { Button, FormControl, Typography, Snackbar, FormHelperText, FormControlLabel, Radio, RadioGroup, TextField, Select, MenuItem, InputLabel, Box, Alert } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import TextFormArea from '../../components/TextFormArea';
 import { Formik, Form, ErrorMessage } from 'formik';
@@ -21,6 +21,7 @@ const HomeScreen = () => {
     currency: 'EUR',
     amount: ''
   });
+  const [fieldError, setFieldError] = useState(false);
   const handleCloseSnackbar = () => {
     setOpenSnackbar(false);
   };
@@ -28,13 +29,25 @@ const HomeScreen = () => {
     const { name, value } = event.target;
     setOffer({ ...offer, [name]: value });
   };
-
+  const handleUsernameChange = (e) => {
+    setUserName(e.target.value);
+    setFieldError(e.target.value === ''); // Set error based on username length
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setFieldError(e.target.value === ''); // Set error based on username length
+  };
   const handleCurrencyChange = (event) => {
     setOffer({ ...offer, currency: event.target.value });
   };
-  
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (userName === '') {
+      setFieldError(true); // Set error if username is empty on form submission
+      return;
+    }
+
     try {
       const formData = {
         userName,
@@ -43,7 +56,7 @@ const HomeScreen = () => {
         message,
         offer,
       };
-  
+
       const response = await axios.post('http://localhost:3001/api/contact', formData);
       setSuccessMessage('Form data submitted successfully!');
       setOpenSnackbar(true);
@@ -58,13 +71,15 @@ const HomeScreen = () => {
         amount: ''
       });
       // Optionally, reset form fields here if needed
-  
+
     } catch (error) {
       console.error('Error Sending Form Data:', error);
       // Handle error scenarios
     }
+
+
   };
-  
+
 
   return (
     <>
@@ -72,102 +87,108 @@ const HomeScreen = () => {
         Contact Form
       </Typography>
       <HomeContainer>
-      <form onSubmit={handleSubmit}>
-        <FormControl >
-          <InputForm
-            value={userName}
-            id="name"
-            label="Name"
-            variant="outlined"
-            type={"text"}
-        
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <InputForm
-            id="email"
-            value={email}
-            label="Email"
-            variant="outlined"
-           type={"email"}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <InputForm
-            id="mobile"
-            value={mobile}
-            label="Mobile"
-            variant="outlined"
-            type={"number"}
-            onChange={(e) => setMobile(e.target.value)}
-          />
-          <TextFormArea
-            id="message"
-            value={message}
-            label="Message"
-            minRows={5}
-            
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <Box>
+        <form onSubmit={handleSubmit}>
+          <FormControl >
+            <InputForm
+              value={userName}
+              id="name"
+              label="Name"
+              variant="outlined"
+              type={"text"}
+              isRequired={fieldError}
+              onChange={handleUsernameChange}
+              error={userName.length === 0} // Example validation for error
+              helperText={userName.length === 0 ? 'Name is required' : ''}
+            />
 
-          
-           <Typography variant="h6" sx={{  marginBottom: '1rem', display: 'flex',  }}> Reason : </Typography>
-        <RadioGroup
-          name="reason"
-          value={offer.reason}
-          onChange={handleInputChange}
-        >
-          <FormControlLabel value="General" control={<Radio />} label="General" />
-          <FormControlLabel value="Buy Your website/Domain" control={<Radio />} label="Buy Your website/Domain" />
-          <FormControlLabel value="Complaint" control={<Radio />} label="Complaint" />
-        </RadioGroup>
-      
+            <InputForm
+              id="email"
+              value={email}
+              label="Email"
+              variant="outlined"
+              type={"email"}
+              onChange={handleEmailChange}
+              isRequired={fieldError}
+              error={email.length === 0} // Example validation for error
+              helperText={email.length === 0 ? 'Emai is required' : ''}
+            />
+            <InputForm
+              id="mobile"
+              value={mobile}
+              label="Mobile"
+              variant="outlined"
+              type={"number"}
+              onChange={(e) => setMobile(e.target.value)}
+            />
+            <TextFormArea
+              id="message"
+              value={message}
+              label="Message"
+              minRows={5}
 
-      {offer.reason === 'Buy Your website/Domain' && (
-        <div>
-          <Typography id="currency-label" variant="h6" sx={{  marginBottom: '1rem', display: 'flex',  }}>  Currency: </Typography>
-          {/* <InputLabel >Currency:</InputLabel> */}
-          <Select
-            labelId="currency-label"
-            id="currency"
-            name="currency"
-            value={offer.currency}
-            onChange={handleCurrencyChange}
-          >
-            <MenuItem value="EUR">EUR</MenuItem>
-            <MenuItem value="USD">USD</MenuItem>
-            <MenuItem value="GBP">GBP</MenuItem>
-            <MenuItem value="CHF">CHF</MenuItem>
-            <MenuItem value="CNY">CNY</MenuItem>
-            <MenuItem value="JPY">JPY</MenuItem>
-          </Select>
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Box>
 
-          <TextField
-            id="amount"
-            name="amount"
-            label="Amount"
-            value={offer.amount}
-            onChange={handleInputChange}
-          />
-        </div>
-      )}
 
-</Box>
-          <Button endIcon={<SendIcon />} color="success" type="submit" variant="contained" sx={{ padding: '1rem', borderRadius: '15px', marginTop: '1rem' }}>
-            Send
-          </Button>
-        </FormControl>
+              <Typography variant="h6" sx={{ marginBottom: '1rem', display: 'flex', }}> Reason : </Typography>
+              <RadioGroup
+                name="reason"
+                value={offer.reason}
+                onChange={handleInputChange}
+              >
+                <FormControlLabel value="General" control={<Radio />} label="General" />
+                <FormControlLabel value="Buy Your website/Domain" control={<Radio />} label="Buy Your website/Domain" />
+                <FormControlLabel value="Complaint" control={<Radio />} label="Complaint" />
+              </RadioGroup>
+
+
+              {offer.reason === 'Buy Your website/Domain' && (
+                <div>
+                  <Typography id="currency-label" variant="h6" sx={{ marginBottom: '1rem', display: 'flex', }}>  Currency: </Typography>
+                  {/* <InputLabel >Currency:</InputLabel> */}
+                  <Select
+                    labelId="currency-label"
+                    id="currency"
+                    name="currency"
+                    value={offer.currency}
+                    onChange={handleCurrencyChange}
+                  >
+                    <MenuItem value="EUR">EUR</MenuItem>
+                    <MenuItem value="USD">USD</MenuItem>
+                    <MenuItem value="GBP">GBP</MenuItem>
+                    <MenuItem value="CHF">CHF</MenuItem>
+                    <MenuItem value="CNY">CNY</MenuItem>
+                    <MenuItem value="JPY">JPY</MenuItem>
+                  </Select>
+
+                  <TextField
+                    id="amount"
+                    name="amount"
+                    label="Amount"
+                    value={offer.amount}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              )}
+
+            </Box>
+            <Button endIcon={<SendIcon />} color="success" type="submit" variant="contained" sx={{ padding: '1rem', borderRadius: '15px', marginTop: '1rem' }}>
+              Send
+            </Button>
+          </FormControl>
         </form>
         <Snackbar
-        open={openSnackbar}
-        autoHideDuration={3000}
-        onClose={handleCloseSnackbar}
-        
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-          <Alert  severity="success">{successMessage}</Alert>
+          open={openSnackbar}
+          autoHideDuration={3000}
+          onClose={handleCloseSnackbar}
+
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        >
+          <Alert severity="success">{successMessage}</Alert>
         </Snackbar>
       </HomeContainer>
     </>
